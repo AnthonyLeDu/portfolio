3665afb85d30a1afad9f1c02b23b8f9d25360c0e
AnthonyLeDu <anthony.le.du@gmail.com>
2023-02-16 22:50:36 +0100

    Moving eslint to airbnb config and fixing code accordingly

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 function createDOMElement(tag, parent, attributes) {
   const element = document.createElement(tag);
   parent.appendChild(element);
-  for (const attr in attributes) {
+  Object.keys(attributes).forEach((attr) => {
     element[attr] = attributes[attr];
-  }
+  });
   return element;
 }
 
@@ -11,7 +11,7 @@ const app = {
 
   state: {
     technos: null,
-    projects: null
+    projects: null,
   },
 
   init() {
@@ -27,16 +27,14 @@ const app = {
       const response2 = await fetch('../data/projects.json');
       app.state.projects = await response2.json();
     } catch (error) {
-      console.error('Error while fetching data : ' + error);
+      console.error(`Error while fetching data : ${error}`);
     }
   },
 
   async updateUI() {
     if (app.state.technos === null) {
       await app.fetchData();
-      app.state.technos = app.state.technos.map(tech => {
-        return { ...tech, active: true };
-      });
+      app.state.technos = app.state.technos.map((tech) => ({ ...tech, active: true }));
     }
     app.updateFilters();
   },
@@ -51,27 +49,27 @@ const app = {
   updateFilters() {
     const filterContainerElement = document.getElementById('filter');
     filterContainerElement.textContent = ''; // Clear
-    for (const techno of app.state.technos) {
+    app.state.technos.forEach((techno) => {
       const activeClass = techno.active ? 'filter-button--active' : '';
       // Create the button
       const buttonContainerElem = createDOMElement('div', filterContainerElement, {
-        'className': `filter-button ${activeClass}`,
+        className: `filter-button ${activeClass}`,
       });
       // Click handler
       buttonContainerElem.addEventListener('click', app.handleFilterClicked(techno));
       // Icon and text
       createDOMElement('img', buttonContainerElem, {
-        'src': `img/icons/${techno.icon}`,
-        'className': 'filter-button__icon'
+        src: `img/icons/${techno.icon}`,
+        className: 'filter-button__icon',
       });
-      createDOMElement('p', buttonContainerElem, { 'textContent': techno.name });
-    }
+      createDOMElement('p', buttonContainerElem, { textContent: techno.name });
+    });
     app.updateProjects();
   },
 
   updateProjects() {
 
-  }
-}
+  },
+};
 
 app.init();
