5083c779fe95c38665af4e9efe004029644c7436
AnthonyLeDu <anthony.le.du@gmail.com>
2023-02-27 18:07:48 +0100

    Always redirecting to home page

--- application/index.js
+++ application/index.js
@@ -3,9 +3,17 @@ const { errorsLogger, mainLogger } = require('./middlewares/loggers');
 require('dotenv').config();
 
 const app = express();
+
+// Loggers
 app.use(errorsLogger);
 app.use(mainLogger);
+
 app.use(express.static('public'));
+
+// All routes not matching anything in public will be redirected to index.html
+app.use((req, res) => {
+  res.redirect('/');
+});
 
 app.set('port', process.env.PORT || 3000);
 app.set('base_url', process.env.BASE_URL);
